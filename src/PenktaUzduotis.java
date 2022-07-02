9f7362784ab7ce81f3f47686cadccd5b203bd503
Vytautas-T <105976888+Vytautas-T@users.noreply.github.com>
2022-07-02 18:28:25 +0300

    atnaujinta

--- src/PenktaUzduotis.java
+++ src/PenktaUzduotis.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 
 public class PenktaUzduotis {
     public static void main(String[] args) {
-        ArrayList<Integer> skaitytuvas = nuskaitytiSarasa();
-        ArrayList<Integer> skaiciai = new ArrayList<>();
+        ArrayList<Integer> skaiciai = nuskaitytiSarasa();
         /** skaiciai.add(1);
          skaiciai.add(2);
          skaiciai.add(3);
@@ -21,16 +20,24 @@ public class PenktaUzduotis {
     public static void atsakymas(ArrayList<Integer> skaiciai) {
         if (liginiuIrNelyginiuSkaiciuDauguma(skaiciai) == 0) {
             if (neLyginiaiSkaiciaiSuma(skaiciai) > lyginiaiSkaiciaiSuma(skaiciai)) {
-                System.out.println("lyginiu ir nelyginiu skaiciu yra vienodai bet nelyginiu skaiciu suma yra didesne  " + "nelyginius kaiciu suma yra - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginius skaiciu suma yra - " + lyginiaiSkaiciaiSuma(skaiciai));
+                System.out.println("lyginiu ir nelyginiu skaiciu yra vienodai bet nelyginiu skaiciu suma yra didesne  " + "nelyginiu kaiciu suma yra - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginius skaiciu suma yra - " + lyginiaiSkaiciaiSuma(skaiciai));
             } else {
                 System.out.println("lyginiu ir nelyginiu skaiciu yra vienodai bet lyginiu skaiciu suma yra didesne  " + "nelyginiu skaiciu suma yra - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginiu skaiciu suma yra - " + lyginiaiSkaiciaiSuma(skaiciai));
             }
-            if (liginiuIrNelyginiuSkaiciuDauguma(skaiciai) == 1) {
-                if (neLyginiaiSkaiciaiSuma(skaiciai) > lyginiaiSkaiciaiSuma(skaiciai)) {
-                    System.out.println("lyginius skaiciu yra daugiau bet nelyginiu suma yra didesne  " + "nelyginiu skaiciu suma yra - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginiu skaiciu suma yra - " + lyginiaiSkaiciaiSuma(skaiciai));
-                } else {
-                    System.out.println("lyginius skaiciu yra daugiau ir lyginiu skaiciu suma yra didesne  " + "nelyginiu skaiciu suma - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginiu skaiciu suma - " + lyginiaiSkaiciaiSuma(skaiciai));
-                }
+        }
+
+        if (liginiuIrNelyginiuSkaiciuDauguma(skaiciai) == 1) {
+            if (neLyginiaiSkaiciaiSuma(skaiciai) > lyginiaiSkaiciaiSuma(skaiciai)) {
+                System.out.println("lyginius skaiciu yra daugiau bet nelyginiu suma yra didesne  " + "nelyginiu skaiciu suma yra - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginiu skaiciu suma yra - " + lyginiaiSkaiciaiSuma(skaiciai));
+            } else {
+                System.out.println("lyginius skaiciu yra daugiau ir lyginiu skaiciu suma yra didesne  " + "nelyginiu skaiciu suma - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginiu skaiciu suma - " + lyginiaiSkaiciaiSuma(skaiciai));
+            }
+        }
+        if (liginiuIrNelyginiuSkaiciuDauguma(skaiciai) == -1) {
+            if (neLyginiaiSkaiciaiSuma(skaiciai) > lyginiaiSkaiciaiSuma(skaiciai)) {
+                System.out.println("nelyginius skaiciu yra daugiau bet nelyginiu suma yra didesne  " + "nelyginiu skaiciu suma yra - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginiu skaiciu suma yra - " + lyginiaiSkaiciaiSuma(skaiciai));
+            } else {
+                System.out.println("nelyginius skaiciu yra daugiau ir lyginiu skaiciu suma yra didesne  " + "nelyginiu skaiciu suma - " + neLyginiaiSkaiciaiSuma(skaiciai) + "  lyginiu skaiciu suma - " + lyginiaiSkaiciaiSuma(skaiciai));
             }
         }
     }
@@ -83,7 +90,7 @@ public class PenktaUzduotis {
         int n = skaitytuvas.nextInt();
 
         for (int i = 0; i < n; i++) {
-            System.out.println("Iveskite " + i + " saraso elimenta");
+            System.out.println("Iveskite " + i + " saraso elementa");
             skaiciai.add(skaitytuvas.nextInt());
         }
         return skaiciai;
